1fd799d14176f999bcc0e5512467fd4608f29969
Fernando Camargo <fernando.camargo@sellics.com>
2018-08-09 11:37:22 +0200

    Fulfilling sales table

--- src/components/features/profit/overview/render.js
+++ src/components/features/profit/overview/render.js
@@ -1,10 +1,15 @@
-import React from "react";
+import property from "lodash/property";
+import React, { Fragment } from "react";
 
 import Form from "components/form";
 import Widget from "components/widget";
 import Table from "components/table";
 
 export const Chart = () => <p>This is a chart</p>;
+
+export const Currency = ({ children }) => children;
+
+export const Percentage = ({ children }) => children;
 
 export default ({ className, sales }) => (
   <div className={className}>
@@ -20,7 +25,49 @@ export default ({ className, sales }) => (
         rows={sales.results}
         columns={[
           { label: () => "starred", content: ({ starred }) => String(starred) },
-          { label: "ASIN", content: ({ asin }) => asin }
+          { label: "ASIN", content: ({ asin }) => asin },
+          {
+            label: "Sales",
+            content: ({ sales: { value, percentage } }) => (
+              <Percentage value={percentage}>
+                <Currency>{value}</Currency>
+              </Percentage>
+            )
+          },
+          { label: "Orders", content: property("orders") },
+          { label: "Refunds", content: property("refunds") },
+          {
+            label: "Profit",
+            content: ({ profit: { value, percentage } }) => (
+              <Percentage value={percentage}>
+                <Currency>{value}</Currency>
+              </Percentage>
+            )
+          },
+          {
+            label: "Margin",
+            content: ({ profit: { value, percentage } }) => (
+              <Percentage value={percentage}>
+                <Currency>{value}</Currency>
+              </Percentage>
+            )
+          },
+          {
+            label: () => "Details",
+            content: () => (
+              <Fragment>
+                <a href="" title="Link to Amazon">
+                  Amazon
+                </a>
+                <a href="" title="Calculate something">
+                  Calculator
+                </a>
+                <a href="" title="See charts">
+                  Charts
+                </a>
+              </Fragment>
+            )
+          }
         ]}
       />
     </Widget>
